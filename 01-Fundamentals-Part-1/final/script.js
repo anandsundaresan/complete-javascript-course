5855d22d834135e294efb3c889f2580e457098e2
anandsundaresan <32130378+anandsundaresan@users.noreply.github.com>
2022-11-26 10:08:40 +0530

    Update script.js

--- 01-Fundamentals-Part-1/final/script.js
+++ 01-Fundamentals-Part-1/final/script.js
@@ -245,7 +245,7 @@ console.log(String(23), 23);
 // type coercion
 // + operator converts number to string
 console.log('I am ' + 23 + ' years old');
-// - operator converts string to number
+// - * / operators convert strings to numbers
 console.log('23' - '10' - 3);
 console.log('23' / '2');
 
